f1abaa4a56240cb16bb64051dad2d6a6437b856a
Shivanarayan Shailesh <shivathelegend2006@gmail.com>
2026-01-20 00:09:10 +0530

    Time: 0 ms (100%), Space: 17.1 MB (89.74%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.c
@@ -0,0 +1,12 @@
+void reverseString(char* s, int sSize) {
+    int left=0;
+    int right = sSize - 1;
+    char temp;
+    while (left < right){
+        temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        right--;
+        left++;
+    }
+}
