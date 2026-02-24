3192f516b288456dbb08fe91b1cc4b94e8cf5741
Shivanarayan Shailesh <shivathelegend2006@gmail.com>
2026-02-24 23:06:58 +0530

    Time: 10 ms (65.48%), Space: 11.1 MB (79.67%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -6,12 +6,13 @@
  * };
  */
 bool hasCycle(struct ListNode *head) {
-    int count = 1;
-    struct ListNode *curr = head;
-    while(curr != NULL){
-        count++;
-        curr = curr->next;
-        if (count > 10001)
+    struct ListNode *step = head;
+    struct ListNode *double_step = head;
+
+    while(double_step != NULL && double_step->next != NULL){
+        step = step->next;
+        double_step = double_step->next->next;
+        if (step == double_step)
             return true;
     }
     return false;
