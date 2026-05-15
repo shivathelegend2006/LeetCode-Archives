15f24b6c63d4bfca574f67bd3e4d8afa161053ab
Shivanarayan Shailesh <shivathelegend2006@gmail.com>
2026-05-15 11:18:34 +0530

    Time: 364 ms (53.42%), Space: 12.6 MB (21.96%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,18 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        n = len(s)
-        dp = [[False]*n for i in range(n)]
-        pal = ""
-        max = 0
+        def expand(l,r):
+            while l >=0 and r <len(s) and s[l] == s[r]:
+                l -= 1
+                r +=1
+            return s[l+1:r]
 
-        for i in range(n-1,-1,-1):
-            for j in range (i,n):
-                if s[i]==s[j] and (j-i <= 1 or dp[i+1][j-1]==True):
-                    dp[i][j] = True
-                    curr = j-i+1
-                    if curr > max:
-                        max = curr
-                        pal = s[i:j+1]
-        return pal
-        
+        longest = ""
+        for i in range(len(s)):
+            odd = expand(i,i)
+            even = expand(i,i+1)
+
+            if len(odd) > len(longest):
+                longest = odd
+            if len(even) > len(longest):
+                longest = even
+
+        return longest
