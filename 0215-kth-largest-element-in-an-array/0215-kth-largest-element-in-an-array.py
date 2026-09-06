945ac5494f918fbbc872dc2d8952cb00d8f1d21f
Shivanarayan Shailesh <shivathelegend2006@gmail.com>
2026-09-06 10:42:33 +0530

    Time: 99 ms (52.98%), Space: 30.9 MB (70.46%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -8,3 +8,14 @@ class Solution:
                 heapq.heappop(min_heap)
 
         return heapq.heappop(min_heap)
+
+        '''
+        the most direct approach is using min heap
+        min heap automaticallys tores such that the binary tree all parents will be lesser than children
+
+        so first store till k elements are filled
+        next for the ith element kick out the smallst and replace with the ith if i is > k which ensure at every step the k most largets only stays in the min heap
+
+        finally return the top of the tree as the kth larghest
+        u can do it without heap but hep is the most direct approach
+        '''
