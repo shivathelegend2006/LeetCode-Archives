64ebb134484e2a06a6afa4893001b55b9c85d105
Shivanarayan Shailesh <shivathelegend2006@gmail.com>
2026-09-19 14:11:44 +0530

    Time: 2 ms (98.65%), Space: 28.8 MB (30.92%) - LeetHub

---
+++ 0355-design-twitter/0355-design-twitter.py
@@ -0,0 +1,50 @@
+import collections
+import heapq
+
+class Twitter:
+    def __init__(self):
+
+        self.time = 0
+        self.tweetMap = collections.defaultdict(list)  
+        self.followMap = collections.defaultdict(set)  
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+
+        self.followMap[userId].add(userId)
+
+        self.tweetMap[userId].append((self.time, tweetId))
+        self.time -= 1
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followMap[followerId]:
+            self.followMap[followerId].remove(followeeId)
+
+    def getNewsFeed(self, userId: int) -> list[int]:
+        res = []
+        minHeap = []
+
+        for followeeId in self.followMap[userId]:
+            if followeeId in self.tweetMap:
+
+                index = len(self.tweetMap[followeeId]) - 1
+                tweet_time, tweetId = self.tweetMap[followeeId][index]
+                
+
+                minHeap.append((tweet_time, tweetId, followeeId, index - 1))
+        
+
+        heapq.heapify(minHeap)
+
+
+        while minHeap and len(res) < 10:
+            tweet_time, tweetId, followeeId, index = heapq.heappop(minHeap)
+            res.append(tweetId)
+
+            if index >= 0:
+                next_time, next_tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush(minHeap, (next_time, next_tweetId, followeeId, index - 1))
+                
+        return res
